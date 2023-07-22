3dfd3517f5e62b664248736caa38bc097ae105f8
Youssef4800 <113474352+Youssef4800@users.noreply.github.com>
2023-07-23 02:50:41 +0300

    YA_Create_A_New_String.cpp

---
+++ Problems/A/YA_Create_A_New_String.cpp
@@ -0,0 +1,13 @@
+//Youssef Amr
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+    string s,t;
+    cin >> s >>t;
+    cout << s.size() << ' ' << t.size() << '\n';
+    cout << s << ' ' << t;
+    return 0;
+
+}
