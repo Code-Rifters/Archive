1e66d22ca1dc1bba85b6b3b02d28674b0637029d
yasmin_1410 <131888042+yasmin-1410@users.noreply.github.com>
2023-08-01 23:53:07 +0300

    YK_Problem_A.cpp

---
+++ Assignments/Team_4/YK_Problem_A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    // yasmin kamal ahmed
+    //problem A
+    int t,x,N;
+    cin>>t;
+    while (t>0)
+    {
+        cin>>x>>N;
+        cout<<pow(x,N)<<endl;
+        t--;
+    }  
+}
