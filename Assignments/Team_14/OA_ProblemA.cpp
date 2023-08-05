fcf9e41124f10d69976e0985476317202fa84196
The Engineer <132500641+OmarAyman2030@users.noreply.github.com>
2023-08-05 16:23:14 +0300

    OA_ProblemA.cpp

---
+++ Assignments/Team_14/OA_ProblemA.cpp
@@ -0,0 +1,12 @@
+  #include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+int num,power,t;
+cin>>t;
+while (t--){
+cin>>num>>power;
+cout<<pow(num,power)<<endl;
+}
+    return 0;
+}
