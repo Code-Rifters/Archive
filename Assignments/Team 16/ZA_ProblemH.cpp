4e9ba817cfb469095e287269c2bb7e0e5e8ae28b
Zyad Amr <115494171+zizuuu22@users.noreply.github.com>
2023-08-05 20:19:13 +0300

    Create  ZA_ProblemH.cpp

---
+++ Assignments/Team 16/ZA_ProblemH.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int x) {
+    if (x == 0 || x == 1) {
+        return 1;
+    }
+    return x * factorial(x - 1);
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; ++i) {
+        int x;
+        cin >> x;
+        int result = factorial(x);
+        cout << result << endl;
+    }
+
+    return 0;
+}
