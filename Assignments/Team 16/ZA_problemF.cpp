fbdd30b989a16fd36a8b4123cc2810764e45997a
Zyad Amr <115494171+zizuuu22@users.noreply.github.com>
2023-08-05 20:17:10 +0300

    Create ZA_problemF.cpp

---
+++ Assignments/Team 16/ZA_problemF.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string input_str;
+    getline(cin, input_str);
+
+    for (char& c : input_str) {
+        c = toupper(c);
+    }
+
+    cout << input_str << endl;
+
+    return 0;
+}
