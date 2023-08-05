1e9767c6f69961e775dc255967616b15ce3d468b
AmiraHassan2003 <90963846+AmiraHassan2003@users.noreply.github.com>
2023-08-05 13:49:00 +0300

    Create AH_problem_A.c

---
+++ Assignments/Team_6/AH_problem_A.c
@@ -0,0 +1,21 @@
+//Amira Hassan
+#include <stdio.h>
+unsigned long long int pow(int x , int n)
+{
+    unsigned long long int result = 1;
+    while(n != 0){
+        result *= x;
+        n--;
+    }
+    return result;
+}
+int main()
+{
+    int t , x , n;
+    scanf("%d" , &t);
+    for(int i=0; i<t; i++){
+            scanf("%d%d" , &x , &n);
+            printf("%llu\n" , pow(x , n));
+    }
+    return 0;
+}
